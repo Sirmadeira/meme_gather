052522769f335fe3c61f74e7ef41a5591243ac89
Sirmadeira <nicolaumori@gmail.com>
2025-12-22 11:49:48 -0300

    Feat: Now when i stop shitting mysef i will make it so we check siblings

--- web_scraper.py
+++ web_scraper.py
@@ -13,9 +13,19 @@ async def main():
         await tab.go_to('https://www.reddit.com/r/dankmemes/best/')
         element = await tab.find(class_name="post-background-image-filter")
         image = element.get_attribute("src")        
-        print(image)
+        # print(image)
 
-        await tab.execute_script("window.scrollBy(0, 1000)")
+
+        number = await tab.find(tag_name="faceplate-number")
+        number_2 =  number.get_attribute("number")
+
+        print(number_2)
+
+        # upvote_container = await tab.query('data-post-click-location="vote"')
+        # print(upvote_container)
+
+        # descendants = await upvote_container.get_children_elements(max_depth=2)
+        # await tab.execute_script("window.scrollBy(0, 1000)")
 
         await asyncio.sleep(1000000)
 
@@ -27,6 +37,3 @@ async def main():
 if __name__ == "__main__":
     asyncio.run(main())
 
-
-# absolute top-1/2 -translate-y-1/2 left-0 w-full h-full opacity-30 object-cover scale-[1.2] post-background-image-filter
-# Class for images
