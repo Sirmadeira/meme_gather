6dd7b6e548efd0c6f55a0f453c3468fba9d36fd1
Sirmadeira <nicolaumori@gmail.com>
2025-12-26 12:00:22 -0300

    Feat: Succesfully loading json

--- web_scraper.py
+++ web_scraper.py
@@ -1,7 +1,9 @@
 import asyncio
+import orjson
 from pydoll.browser import Chrome
 from pydoll.browser.options import ChromiumOptions
 
+# https://www.reddit.com/r/news.json?limit=100
 async def main():
     options = ChromiumOptions()
     options.binary_location = "/usr/bin/chromium"
@@ -9,44 +11,13 @@ async def main():
 
     async with Chrome(options=options) as browser:
         tab = await browser.start()
-        await tab.go_to('https://www.reddit.com/r/dankmemes/best/')
-        await tab.go_to('https://www.reddit.com/r/dankmemes/best/')
-
-
-
-
-        title_element = await tab.query("//a//faceplate-screen-reader-content")
-        print(await title_element.text)
-
-
-
-
-        image_element = await tab.find(class_name="post-background-image-filter")
-        image = image_element.get_attribute("src")        
-        print(image)
-
-
-        upvote_number = await tab.query("//span//faceplate-number[@number]",find_all=True,timeout=5)
-        print(upvote_number)
-
-
-
-
-
-        # upvote_container = await tab.query('data-post-click-location="vote"')
-        # print(await upvote_container.inner_html)
-
-        # descendants = await upvote_container.get_children_elements(max_depth=2)
-
-
-        await tab.execute_script("window.scrollBy(0, 1000)")
-        await asyncio.sleep(1000000)
-
-
-
-        browser.close()
-
+        await tab.go_to("https://www.reddit.com/r/news.json?limit=1")
+        json_holder = await tab.find(tag_name="pre")
+        print(await json_holder.text)
+        print(orjson.loads(await json_holder.text))
+        
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    raw_json = asyncio.run(main())
+
 
